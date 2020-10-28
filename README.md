98641d3822234b370543ac2edfd6d9cc60cd5543
James Lynch <lynchjames@gmail.com>
2020-10-28 01:15:32 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# note-refactor-obsidian
+
+This repository contains a plugin for [Obsidian](https://obsidian.md/) for extracting the selected portion of a notes into new note. 
+
+The defaults hotkey is:
+
+| Hotkey                                            | Action                                     |
+| ------------------------------------------------- | ------------------------------------------ |
+| <kbd>Ctrl/Cmd</kbd> <kbd>Shift</kbd> + <kbd>N</kbd>                    | Copy selection into new note and replace with a link.                 |
+
+The new file is created at the root of the vault.
+
+Headings `#` and other illegal characters for filenames are removed including `:`, `\`, `/`.
+
+A check is done to avoid overwriting of existing files but...
+
+    💥 PLEASE TRY IN A TEST VAULT FIRST! 💥
+
+## Demo
+
+TODO
+
+## Installing
+
+TODO
---
+++ main.ts
@@ -0,0 +1,62 @@
+import {
+  App,
+  MarkdownView,
+  Notice,
+  Plugin,
+} from "obsidian";
+
+export default class NoteRefactor extends Plugin {
+  onInit() {}
+
+  onload() {
+    console.log("Loading Note Refactor plugin");
+
+    this.addCommand({
+      id: 'app:extract-selection',
+      name: 'Extract selection to new note',
+      callback: () => this.extractSelection(),
+      hotkeys: [
+        {
+          modifiers: ["Mod", "Shift"],
+          key: "n",
+        },
+      ],
+    });
+  }
+
+  onunload() {
+    console.log("Unloading Note Refactor plugin");
+  }
+
+  extractSelection(): void {
+      const markdownSourceView = this.app.workspace.activeLeaf.getViewState();
+      const mdView = this.app.workspace.activeLeaf.view as MarkdownView;
+      if(!mdView) {return}
+      const doc = mdView.sourceMode.cmEditor;
+      const selectedText = doc.getSelection()
+
+      if (!selectedText) { return }
+
+      const [header, ...contentArr] = selectedText.split('\n')
+
+      const rootFolder = this.app.vault.getRoot();
+
+      let headerRegex = /[:\/\\#]/gim;
+
+      //TODO: Use file path settings
+      let fileName = header.replace(headerRegex, '').trim();
+      let filePath = fileName + '.md';
+      this.app.vault.adapter.exists(filePath, false).then((exists) => {
+        if(exists){
+          console.log('File exists!')
+          new Notice(`A file namde ${fileName} already exists`);
+          return;
+        } else {
+          this.app.vault.create(filePath, contentArr.join('\n').trim()).then((newFile) => {
+            doc.replaceSelection(`[[${fileName}]]`);
+            this.app.workspace.openLinkText(fileName, filePath, true);
+          });
+        }
+      });
+  }
+}
