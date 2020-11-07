869035d2bf2a7c10f15000b61c47f8e3777a5194
James Lynch <lynchjames@gmail.com>
2020-11-07 19:00:12 +0000

    Dev build copied into test vault using rollup-copy

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import typescript from '@rollup/plugin-typescript';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import copy from 'rollup-plugin-copy';
+const TEST_VAULT = 'test-vault/.obsidian/plugins/note-refactor-obsidian';
 
 export default {
   input: 'src/main.ts',
@@ -15,5 +17,11 @@ export default {
     typescript(),
     nodeResolve({browser: true}),
     commonjs(),
+    copy({
+      targets: [
+        { src: 'dist/main.js', dest: TEST_VAULT },
+        { src: ['manifest.json', 'styles.css'], dest: TEST_VAULT }
+      ], flatten: true
+    })
   ]
 };
